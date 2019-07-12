880ca3efa465e47024bb5d1e2569cd5c3a6b7098
Lain Musgrove <lain.proliant@gmail.com>
2019-07-12 14:44:56 -0700

    Added debug.h, with debugger and dbgprint function/macro.

---
+++ include/moonlight/debug.h
@@ -0,0 +1,30 @@
+/*
+ * moonlight/debug.h: Functions and macros for debugging purposes.
+ *
+ * Author: Lain Supe (lainproliant)
+ * Date: Friday, July 12 2019
+ */
+#pragma once
+
+#include "moonlight/core.h"
+
+//-------------------------------------------------------------------
+#ifdef MOONLIGHT_DEBUG
+#include <csignal>
+#define debugger raise(SIGSEGV)
+#else
+#define debugger (void)0
+#endif
+
+//-------------------------------------------------------------------
+#ifdef MOONLIGHT_DEBUG
+#include <tinyformat/tinyformat.h>
+template<typename... Args>
+inline void dbgprint(const std::string& fmt, const Args&... args) {
+   tfm::format(std::cerr, fmt, args...);
+}
+
+#else
+#define dbgprint(...) (void)0
+
+#endif
