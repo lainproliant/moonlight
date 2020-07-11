f994ee12894a0e50d5fa9ac11da939c6df800825
Lain Musgrove <lain.proliant@gmail.com>
2020-07-11 02:30:06 -0700

    Update for panifex 1.0 removing class-module builds.

--- bake.py
+++ bake.py
@@ -6,17 +6,21 @@
 # --------------------------------------------------------------------
 #
 from pathlib import Path
-from panifex import build, sh, default
+from panifex import build, sh, default, target, provide
 
 # -------------------------------------------------------------------
-sh.env(CC="g++",
-       CFLAGS=("-g",
-               "-I./include",
-               "--std=c++2a",
-               "-DMOONLIGHT_DEBUG",
-               "-DMOONLIGHT_ENABLE_STACKTRACE",
-               "-DMOONLIGHT_STACKTRACE_IN_DESCRIPTION"),
-       LDFLAGS=("-rdynamic", "-g", "-lpthread", "-ldl"))
+sh.env(
+    CC="g++",
+    CFLAGS=(
+        "-g",
+        "-I./include",
+        "--std=c++2a",
+        "-DMOONLIGHT_DEBUG",
+        "-DMOONLIGHT_ENABLE_STACKTRACE",
+        "-DMOONLIGHT_STACKTRACE_IN_DESCRIPTION",
+    ),
+    LDFLAGS=("-rdynamic", "-g", "-lpthread", "-ldl"),
+)
 
 
 # -------------------------------------------------------------------
@@ -24,31 +28,39 @@ def compile_test(src):
     return sh(
         "{CC} {CFLAGS} {input} {LDFLAGS} -o {output}",
         input=src,
-        output=Path(src).with_suffix("")
+        output=Path(src).with_suffix(""),
     )
 
 
 # -------------------------------------------------------------------
-@build
-class MoonlightTests:
-    def submodules(self):
-        return sh('git submodule update --init --recursive')
+@provide
+def submodules():
+    return sh("git submodule update --init --recursive")
 
-    def sources(self, submodules):
-        return Path.cwd().glob("test/*.cpp")
 
-    def tests(self, sources):
-        return [compile_test(src) for src in sources]
+# -------------------------------------------------------------------
+@provide
+def sources(submodules):
+    return Path.cwd().glob("test/*.cpp")
 
-    def run_tests(self, tests):
-        return (sh("{input}",
-                   input=test,
-                   cwd="test").interactive()
-                for test in tests)
 
-    @default
-    def run_tests_async(self, tests):
-        return (sh("{input}",
-                   input=test,
-                   cwd="test")
-                for test in tests)
+# -------------------------------------------------------------------
+@provide
+def tests(sources):
+    return [compile_test(src) for src in sources]
+
+
+# -------------------------------------------------------------------
+@target
+def run_tests(tests):
+    return (sh("{input}", input=test, cwd="test").interactive() for test in tests)
+
+
+# -------------------------------------------------------------------
+@default
+def run_tests_async(tests):
+    return (sh("{input}", input=test, cwd="test") for test in tests)
+
+
+# -------------------------------------------------------------------
+build()
