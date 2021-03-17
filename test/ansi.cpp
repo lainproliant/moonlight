df0bcc4772ee998aaf31a330043f1092f60d283e
Lain Musgrove <lain.proliant@gmail.com>
2021-03-17 14:48:00 -0700

    Intermediate commit: API for RGB<>HSV conversions.

--- test/ansi.cpp
+++ test/ansi.cpp
@@ -6,6 +6,11 @@ using namespace moonlight;
 int main(int argc, char** argv) {
    (void) argc;
    (void) argv;
-   std::cout << "Hello from the " << fg::bright::red("U") << fg::bright::white("S") << fg::bright::blue("A") << "!" << std::endl;
+   const auto red = fg::bright(fg::red);
+   const auto white = fg::bright(fg::white);
+   const auto blue = fg::bright(fg::blue);
+   std::cout << "Hello from the " << red("U") << white("S") << blue("A") << "!" << std::endl;
+   std::cout << "This text is " << fg::reverse("reversed") << "." << std::endl;
+   std::cout << "This text might " << fg::blink("blink") << " if your terminal supports it." << std::endl;
    return 0;
 }
