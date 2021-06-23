6e2fd0ddebd3f7f55ba0bbb776a51b1d0a04b4e9
Lain Musgrove <lain.proliant@gmail.com>
2021-06-23 16:13:25 -0700

    Add a lab test for sole::uuid

---
+++ lab/uuid.cpp
@@ -0,0 +1,20 @@
+/*
+ * uuid.cpp
+ *
+ * Author: Lain Musgrove (lain.proliant@gmail.com)
+ * Date: Wednesday June 23, 2021
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "sole/sole.hpp"
+#include <iostream>
+
+int main() {
+    auto uuid = sole::uuid4();
+    std::cout << uuid.pretty() << std::endl;
+    std::cout << uuid.str() << std::endl;
+    std::cout << uuid.base62() << std::endl;
+    std::cout << std::hex << uuid.ab << " " << uuid.cd << std::endl;
+    return 0;
+}
