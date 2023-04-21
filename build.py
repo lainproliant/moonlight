ae1c74bb29b545bd0eeb1507e3c650526cb29e62
Lain Musgrove <lain.proliant@gmail.com>
2023-04-20 23:12:57 -0700

    Updates for xeno v5

--- build.py
+++ build.py
@@ -9,11 +9,14 @@
 # -------------------------------------------------------------------
 
 import os
+import sys
 import random
 import shlex
 from pathlib import Path
 
-from xeno.build import Recipe, build, default, action, provide, sh, target
+from xeno.recipe import Recipe
+from xeno.build import provide, recipe, task, engine
+from xeno.cookbook import sh
 
 INTERACTIVE_TESTS = {"ansi"}
 
@@ -21,86 +24,73 @@ INCLUDES = ["-I./include", "-I./deps/date/include", "-I./deps"]
 
 ENV = dict(
     CC=os.environ.get("CC", "clang++"),
-    CFLAGS=(
-        *shlex.split(os.environ.get("CFLAGS", "")),
-        "-Wall",
-        "-g",
-        "-fpermissive",  # needed for g++ to respect "always_false<T>"
-        *INCLUDES,
-        "-DMOONLIGHT_ENABLE_STACKTRACE",
-        "-DMOONLIGHT_STACKTRACE_IN_DESCRIPTION",
-        "--std=c++2a",
+    CFLAGS=shlex.join(
+        [
+            *shlex.split(os.environ.get("CFLAGS", "")),
+            "-Wall",
+            "-g",
+            "-fpermissive",  # needed for g++ to respect "always_false<T>"
+            *INCLUDES,
+            "-DMOONLIGHT_ENABLE_STACKTRACE",
+            "-DMOONLIGHT_STACKTRACE_IN_DESCRIPTION",
+            "--std=c++2a",
+        ]
     ),
-    LDFLAGS=(*shlex.split(os.environ.get("LDFLAGS", "")), "-g", "-lpthread"),
+    LDFLAGS=(
+        shlex.join([*shlex.split(os.environ.get("LDFLAGS", "")), "-g", "-lpthread"])
+    ),
     PREFIX=os.environ.get("PREFIX", "/usr/local"),
     DESTDIR=os.environ.get("DESTDIR", ""),
     STRESS_CYCLES=os.environ.get("STRESS_CYCLES", "100"),
 )
 
-# -------------------------------------------------------------------
-class StressTest(Recipe):
-    def __init__(self, input, cycles=ENV["STRESS_CYCLES"]):
-        super().__init__([input])
-        self.named(input.name).with_type("stress")
-        self._cycles = int(cycles)
-        self._done = False
-        self._test = input
-
-    async def make(self):
-        for x in range(self._cycles):
-            test = sh("{test}", test=self._test, interactive=True)
-            await test.resolve()
-            assert test.done, f"Stress test failed after {x} iterations."
-        self._done = True
-
-    @property
-    def done(self):
-        return self._done
 
 # -------------------------------------------------------------------
-@provide
+@recipe(sigil=lambda r: f'{r.name}:{r.arg("program").target.name}')
+async def stress_test(program, cycles: int = 10):
+    for x in range(cycles):
+        await sh(program)()
+
+
+# -------------------------------------------------------------------
+@task
 def submodules():
     return sh("git submodule update --init --recursive")
 
 
 # -------------------------------------------------------------------
-@action
+@recipe(factory=True, sigil=lambda r: f'{r.name}:{r.target.name}')
 def compile(src, headers):
-    cmd = "{CC} {CFLAGS} {src} {LDFLAGS} -o {output}"
+    cmd = "{CC} {CFLAGS} {src} {LDFLAGS} -o {target}"
 
-    return sh(
-        cmd,
-        env=ENV,
-        src=src,
-        output=Path(src).with_suffix(""),
-        requires=headers,
-    )
+    return sh(cmd, env=ENV, src=src, target=src.with_suffix(""))
 
 
 # -------------------------------------------------------------------
-@action
-def test(test):
-    return sh(
-        "{test}",
+@recipe(factory=True, sigil=lambda r: f"{r.name}:{r.arg('t').target.name}")
+def test(t):
+    result = sh(
+        t.target,
+        t=t,
         env=ENV,
-        test=test,
-        interactive=test.output.name in INTERACTIVE_TESTS,
+        interactive=t.name in INTERACTIVE_TESTS,
     )
+    return result
 
 
 # -------------------------------------------------------------------
-@action
-def install(program):
-    output = ENV["DESTDIR"] + ENV["PREFIX"] + "/bin/" + program.output.name
+@recipe(factory=True)
+def install(target):
+    path = Path(ENV["DESTDIR"]) / ENV["PREFIX"] / "bin" / target.name
     return sh(
         "mkdir -p {DESTDIR}{PREFIX}/bin; "
-        "cp -f {program} {output}; "
-        "chmod 775 {output}",
+        "cp -f {program} {target}; "
+        "chmod 775 {target}",
         env=ENV,
-        program=program,
-        output=output,
+        program=target,
+        target=path,
         as_user="root",
-    ).named(program.name)
+    )
 
 
 # -------------------------------------------------------------------
@@ -128,58 +118,55 @@ def headers():
 
 
 # -------------------------------------------------------------------
-@target
-async def labs(lab_sources, headers, submodules):
-    await submodules.resolve()
-    return [compile(src, headers).with_prefix("lab.") for src in lab_sources]
+@task(dep="submodules")
+def labs(lab_sources, headers, submodules):
+    return [compile(src, headers) for src in lab_sources]
 
 
 # -------------------------------------------------------------------
-@target
-async def tests(test_sources, headers, submodules):
-    await submodules.resolve()
-    tests = [compile(src, headers).with_prefix("test.") for src in test_sources]
+@task(dep="submodules")
+def tests(test_sources, headers, submodules):
+    tests = [compile(src, headers) for src in test_sources]
     return [*random.sample(tests, len(tests))]
 
 
 # -------------------------------------------------------------------
-@target
-async def utils(util_sources, headers, submodules):
-    await submodules.resolve()
-    utils = [compile(src, headers).with_prefix("util.") for src in util_sources]
+@task
+def utils(util_sources, headers, submodules):
+    utils = [compile(src, headers) for src in util_sources]
     return utils
 
 
 # -------------------------------------------------------------------
-@target
+@task
 def install_utils(utils):
     return (install(util) for util in utils)
 
 
 # -------------------------------------------------------------------
-@default
+@task(default=True)
 def run_tests(tests):
-    return (test(t) for t in tests)
+    return [test(t) for t in tests]
 
 
 # -------------------------------------------------------------------
-@target
+@task
 def assert_tests_stable(tests):
-    return (StressTest(t) for t in tests)
+    return (stress_test(t) for t in tests)
 
 
 # -------------------------------------------------------------------
-@target
-def all(tests, labs, utils, assert_tests_stable):
-    return (tests, labs)
+@task
+def all(tests, labs, utils):
+    return (tests, labs, utils)
 
 
 # -------------------------------------------------------------------
-@target
+@task
 def cc_json():
-    return sh("intercept-build ./build.py compile:\* -R; ./build.py -c compile:\*")
+    return sh("intercept-build ./build.py compile:\\* -R; ./build.py -c compile:\\*")
 
 
 # -------------------------------------------------------------------
 if __name__ == "__main__":
-    build()
+    engine.build(*sys.argv[1:])
