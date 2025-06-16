fbb65ab039e71dd0b187a387b74fd4039e7d1455
Lain Musgrove (lainproliant) <lain.proliant@gmail.com>
2025-06-16 13:44:30 -0700

    Experimental Error object with Rust-like Stacktrace support.

--- include/moonlight/result.h
+++ include/moonlight/result.h
@@ -9,28 +9,94 @@
 #define __MOONLIGHT_RESULT_H
 
 #include <optional>
+#include <string>
+#include <sstream>
+
+#ifdef MOONLIGHT_ENABLE_STACKTRACE
+#include "moonlight/debug.h"
+#endif
 
 namespace moonlight {
 
+// ------------------------------------------------------------------
+template<class T = int>
+class Error {
+public:
+#ifdef MOONLIGHT_ENABLE_STACKTRACE
+    Error(const debug::StackTrace& stack)
+    : _stack(stack) { }
+    Error(const debug::StackTrace& stack, const T& value)
+    : _value(value), _stack(stack) { }
+#else
+    Error() { }
+    Error(const T& value)
+    : _value(value) { }
+#endif
+
+    Error<T>& operator()(const T& value) {
+        _value = value;
+        return *this;
+    }
+
+    const T& value() const {
+        return _value;
+    }
+
+    std::string str() const {
+        std::ostringstream sb;
+        sb << value() << std::endl;
+#ifdef MOONLIGHT_ENABLE_STACKTRACE
+        stack().format(sb, "    at ");
+#endif
+        return sb.str();
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Error& e) {
+        out << e.str();
+        return out;
+    }
+
+#ifdef MOONLIGHT_ENABLE_STACKTRACE
+    const debug::StackTrace& stack() const {
+        return _stack;
+    }
+#endif
+
+private:
+    T _value;
+
+#ifdef MOONLIGHT_ENABLE_STACKTRACE
+    debug::StackTrace _stack;
+#endif
+
+};
+
+// ------------------------------------------------------------------
+#ifdef MOONLIGHT_ENABLE_STACKTRACE
+#define ERROR Error(debug::StackTrace::generate(LOCATION))
+#else
+#define ERROR Error()
+#endif
+
+// ------------------------------------------------------------------
 template<class T, class E>
 class Result {
  public:
+     Result()
+     : _value(T()) { }
+
      Result(const T& value)
      : _value(value) { }
 
-     Result(const E& error)
+     Result(const Error<E>& error)
      : _error(error) { }
 
-     bool has_value() const {
+     bool ok() const {
          return _value.has_value();
      }
 
      const T& value() const {
          return _value.value();
-     }
-
-     bool is_error() const {
-         return _error.has_value();
      }
 
      const E& error() const {
